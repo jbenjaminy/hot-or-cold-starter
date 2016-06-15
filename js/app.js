2a436b996015b26657760fec2c1c47edd2ad50e6
jbenjaminy <young.benjamin@gmail.com>
2016-06-15 14:10:34 -0500

    Adds initial JS functions

--- js/app.js
+++ js/app.js
@@ -12,6 +12,41 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+var hotOrCold = function(){
+// guess count
+	var guessCount = 0;
+// number generator
+	var randomNumb = Math.floor((Math.random() * 100) + 1);
+		// verify that 1 & 100 are included in this range
+// user guess
+	var userGuess = parseInt($('#userGuess').val());
+	if ((userGuess >= 1) && (userGuess <= 100) && (userGuess !== NaN)) {
+		guessCount += 1; 
+			// apply to span#count
+		if (userGuess === randomNumb) {
+			console.log("You got it!")
+		}
+		else if (userGuess != randomNumb) {
+			console.log("Sorry!")
+		}
+// compares to guess
+	else alert("Invalid input, please choose an integer between 1 & 100.")
+	} 
+
+// function giving feedback on proxity of guess; not relative to prev guess, but to correct answer (div#feedback)
+
+// list of prev guesses (each guess set to <li> in ul#guessList)
+
+// ensure HTML5
+
+}
+
+// var newGame = function() {
+// 	// reset # of guesses, feedback section, list of guesses
+// 	// be able to start consecutive games without reloading or refreshing
+// }
+
 });
 
-
+// splitting the code into functions (anywhere that code repeats and to group things that do one thing)
